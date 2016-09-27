603509999a051b339263884ec0a70cf9b69453d2
master <2672889064@qq.com>
2016-09-27 17:29:47 +0800

    .idea

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,8 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.print("HelloWorls!!!");
-
+        System.out.print("\n");
+        System.out.print("you are my angle!!!!\n");
+        System.out.println("最是那一低头的温柔，恰似一朵水莲不胜凉风的娇羞。");
     }
 }
